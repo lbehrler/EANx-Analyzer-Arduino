beebb569af50db49265175f3599319ab505fdf6d
lbehrler <38298678+lbehrler@users.noreply.github.com>
2022-12-06 09:15:38 -0600

    moved board configs to header file

--- EANx_Analyzer_SPI_OLED/EANx_Analyzer_SPI_OLED.ino
+++ EANx_Analyzer_SPI_OLED/EANx_Analyzer_SPI_OLED.ino
@@ -18,67 +18,11 @@
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <Adafruit_ADS1X15.h>  
 #include <splash.h>
-// #include "pin_config.h"
-
-// Chip Specific settings for SPI OLED 
-#if defined(ARDUINO_FEATHER_ESP32) // Feather Huzzah32
-  #define TFT_CS         14
-  #define TFT_RST        15
-  #define TFT_DC         32
-
-#elif defined(ESP8266)
-  #define TFT_CS         4
-  #define TFT_RST        16                                            
-  #define TFT_DC         5
-
-#elif defined(SEEED_XIAO_M0)  // Seeed XAIO
-  #define TFT_CS        6
-  #define TFT_RST       2     // Or set to -1 and connect to Arduino RESET pin
-  #define TFT_DC        7
-  #define TFT_MOSI      10    // Data out
-  #define TFT_SCLK      8     // Clock out
-  #define ADCPIN0       0
-  #define ADCPIN1       1
-  #define ADCFACT       1024  
-
-#elif defined(ARDUINO_ESP32_PICO)
-  #define TFT_SDA       21     
-  #define TFT_SCL       22
-  #define TFT_MOSI      23    // Data out
-  #define TFT_SCLK      18    // Clock out  #define 
-  #define TFT_RST       5     // Or set to -1 and connect to Arduino RESET pin                                            
-  #define TFT_DC        10
-  #define TFT_CS        9
-  #define ADCPIN0       36
-  #define ADCPIN1       39
-  #define ADCFACT       4095 
-
-#elif defined(ARDUINO_AVR_NANO)
-  #define TFT_SDA       23     
-  #define TFT_SCL       24
-  #define TFT_MOSI      11    // Data out
-  #define TFT_SCLK      13    // Clock out  #define 
-  #define TFT_RST       -1    // Or set to -1 and connect to Arduino RESET pin                                            
-  #define TFT_DC        9
-  #define TFT_CS        10
-  #define BUTTON        2
-  #define ADCPIN0       36
-  #define ADCPIN1       39
-  #define ADCFACT       4095 
-
-#else
-  // For the breakout board, you can use any 2 or 3 pins.
-  // These pins will also work for the 1.8" TFT shield.
-  #define TFT_CS        10
-  #define TFT_RST       9 // Or set to -1 and connect to Arduino RESET pin
-  #define TFT_DC        8
-  #define ADCPIN0       0
-  #define ADCPIN1       3
-#endif
+#include "pin_config.h"
 
 // ST1306 definitions
-#define SCREEN_WIDTH  128             // OLED display width, in pixels
-#define SCREEN_HEIGHT 32              // OLED display height, in pixels
+#define SCREEN_WIDTH  240           // OLED display width, in pixels
+#define SCREEN_HEIGHT 240              // OLED display height, in pixels
 #define OLED_RESET     -1             // Reset pin # (or -1 if sharing Arduino reset pin)
 
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST); //Define OLED display
@@ -316,7 +260,7 @@ void printLayout()
 
 void initst7789()
 {
-  tft.init(240, 240);       // Init ST7789 240x240
+  tft.init(SCREEN_WIDTH, SCREEN_HEIGHT);       // Init ST7789 240x240
 //  tft.init(128, 160);       // Init ST7735 128x160
   tft.setRotation(2);       // Adjust SS7789 Orientation
 }
